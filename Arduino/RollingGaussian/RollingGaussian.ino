b41797a19e5c4ac2e2241412f5a31397fffe4220
antNLocks <antn.lorentz@gmail.com>
2022-06-26 19:58:37 +0200

    Various improvements

---
+++ Arduino/RollingGaussian/RollingGaussian.ino
@@ -0,0 +1,38 @@
+byte gaussianBuffer[1008];
+
+void setup() {
+ Serial.begin(250000);
+
+ computeGaussian(3);
+}
+
+
+void loop() {
+  for(int i = 0; i < 21; i++){
+    for(int j = 0; j < 12; j++){
+      serialGaussian(j, i);
+      delay(20); 
+    }
+  }
+
+}
+void computeGaussian(float sigma){
+  for(int i = 0; i < 42; i++){
+    for(int j = 0; j < 24; j++){
+      int d = 255 * exp(-((12 - j)*(12 - j) + (i - 21)*(i - 21))/ (2*sigma*sigma));
+      byte b = constrain(d, 1, 255);
+      gaussianBuffer[42*j+i] = b;
+    }
+  }
+}
+
+void serialGaussian(int x, int y){
+  for(int i = 0; i < 21; i++){
+    for(int j = 0; j < 12; j++){
+      Serial.write(gaussianBuffer[42*(j+x) + i + y]);
+    }
+  }
+  Serial.write(0x00);
+  Serial.flush();
+}
+
